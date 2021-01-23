9f49769d756522b25561b80fccb26676b59070ac
kebufu <31839741+kebufu@users.noreply.github.com>
2021-01-23 13:36:51 +0800

    Add files via upload

---
+++ nbsPlayer.lua
@@ -0,0 +1,74 @@
+local nbs=require("nbs")
+local component=require("component")
+local gpu=component.gpu
+
+local X,Y=gpu.getResolution()
+local args=shell.resolve(...)
+if args==nil then
+  print("nbsPlayer <file>")
+  os.exit(-1)
+end
+local file=nbs.parse_file(args)
+
+local notes={}
+
+for k,v in component.list("iron_noteblock") do
+  table.insert(notes,component.proxy(k))
+end
+if #notes==0 or #notes<file.headers.song_layers then
+  io.stderr:write(string.format("This nbs file need %d iron note blocks,but found %d iron note blocks.\n",file.headers.song_layers,#notes))
+  os.sleep(5)
+end
+setmetatable(notes,{__index=function() return {playNote=function() end} end})
+local ticks={}
+for _,i in ipairs(file.notes) do
+  for j=1,i.tick+1 do
+    if ticks[j]==nil then
+      ticks[j]={}
+    end
+  end
+  for j=1,i.layer+1 do
+    if ticks[i.tick+1][j]==nil then
+      ticks[i.tick+1][j]={}
+    end
+  end
+  ticks[i.tick+1][i.layer+1]={math.max(1,math.min(i.key-33,24)),i.instrument,i.layer+1}
+end
+colors={0x3d3c84,0x98c09c,0xc59d9d,0xbf98c0,0x844587,0x838180,0x7a843c,0x874572,0x457887,0x676767,0}
+gpu.fill(1,1,X,Y," ")
+local function update(tick)
+  gpu.copy(1,1,X,Y,-2,0)
+  local layer=1
+  if ticks[tick] then
+    for k,i in ipairs(ticks[tick]) do
+      if #i~=0 then
+        gpu.setForeground(colors[i[2]+1]|0xFFFFFF)
+        gpu.setBackground(colors[i[2]+1])
+        gpu.set(X-1,layer,string.format("%0.2d",i[1]))
+      else
+        gpu.setBackground(0)
+        gpu.set(X-1,layer,"  ")
+      end
+      layer=layer+1
+    end
+  else
+    gpu.setBackground(0)
+    gpu.fill(X-1,1,2,Y," ")
+  end
+end
+local delay=1/file.headers.tempo
+for i=1,math.ceil(X/2) do
+  update(i)
+  os.sleep(delay)
+end
+local a=math.ceil(X/2)-1
+for j,i in ipairs(ticks) do
+  for _,i in ipairs(i) do
+    if #i~=0 then
+      notes[i[3]+1].playNote(nbs.INSTRUMENTS[i[2]+1],i[1])
+    end
+  end
+  update(j+a)
+  os.sleep(delay)
+end
+require("term").clear()
